97f3c5551df2c8f2d1b1981144744cc47f2785fb
ccbagel <hasan.abdi.ahmed97@gmail.com>
2022-07-02 19:40:25 -0600

    menu and gameList layout complete

--- components/Content.js
+++ components/Content.js
@@ -1,12 +1,16 @@
 // imports
+import Menu from "./Menu";
+import GamesList from "./GamesList";
 
 function Content() {
   return (
-    <div className="flex flex-row items-center overflow-hidden text-white">
-      <h1 className="border-r-white bg-slate-500 h-screen hidden md:block md:w-1/3">
+    <div className="flex flex-row items-start text-white max-h-screen p-5">
+      {/* <h1 className="border-r-white bg-slate-500 h-screen hidden md:block md:w-1/3">
         Menu
-      </h1>
-      <h1 className="bg-neutral-800 w-full h-screen">Games</h1>
+      </h1> */}
+      <Menu />
+      {/* <h1 className="bg-neutral-800 w-full h-screen">Games</h1> */}
+      <GamesList />
     </div>
   );
 }
--- components/GamesList.js
+++ components/GamesList.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function GamesList() {
-  return <div></div>;
+  return (
+    <div className="p-3">
+      <h1 className="text-5xl md:text-6xl font-bold">New & Trending</h1>
+    </div>
+  );
 }
 
 export default GamesList;
